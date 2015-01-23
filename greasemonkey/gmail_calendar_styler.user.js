3d1120bfbc12cd5a0d3114c7935409e6e78a4b68
George Christou <gechrr@gmail.com>
2015-01-23 23:15:14 +0100

    gmail_calendar_styler.user.js: Initial commit

---
+++ greasemonkey/gmail_calendar_styler.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name     Gmail Calendar Styler
+// @author   George Christou
+// @include  https://www.google.com/calendar/mgadget?container=gm*
+// ==/UserScript==
+
+GM_addStyle('\
+  .dp-cell { color: #000 !important; }\
+  .gadget-header { background-color: #f5f5f5 !important; }\
+  .details-container { color: #444 !important; background-color: #fff !important; }\
+  .date-label { color: #fff; background-color: #555 !important; }\
+  .day { color: #444; background-color: #f5f5f5 !important; }\
+  .menu-link { color: #5BA9FF; font-weight: bold; text-decoration: none; }\
+');
