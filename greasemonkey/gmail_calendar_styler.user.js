6858608e86fb828eb16c88dc60ed7aa4ca0f8cda
George Christou <gechrr@gmail.com>
2016-12-08 10:55:14 +0000

    gmail_calendar_styler.user.js: Fix GM_addStyle permissions

--- greasemonkey/gmail_calendar_styler.user.js
+++ greasemonkey/gmail_calendar_styler.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
 // @name     Gmail Calendar Styler
 // @author   George Christou
-// @include  http*://calendar.google.com/calendar/mgadget*
+// @include  https://calendar.google.com/calendar/mgadget*
+// @grant    GM_addStyle
 // ==/UserScript==
 
-GM_addStyle('\
-  .dp-cell { color: #000 !important; }\
-  .gadget-header { background-color: #f5f5f5 !important; }\
-  .details-container { color: #444 !important; background-color: #fff !important; }\
-  .date-label { color: #fff; background-color: #555 !important; }\
-  .day { color: #444; background-color: #f5f5f5 !important; }\
-  .menu-link { color: #5BA9FF; font-weight: bold; text-decoration: none; }\
-');
+GM_addStyle(
+  '.dp-cell { color: #000 !important; }' +
+  '.gadget-header { background-color: #f5f5f5 !important; }' +
+  '.details-container { color: #444 !important; background-color: #fff !important; }' +
+  '.date-label { color: #fff; background-color: #555 !important; }' +
+  '.day { color: #444; background-color: #f5f5f5 !important; }' +
+  '.menu-link { color: #5BA9FF; font-weight: bold; text-decoration: none; }'
+);
