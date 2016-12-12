47c0b078ccc9d94f4944029329cbd93a9aac371d
George Christou <gechrr@gmail.com>
2016-12-12 16:03:54 +0000

    gmail_calendar_styler.user.js: Add `menu-arrow` styling

--- greasemonkey/gmail_calendar_styler.user.js
+++ greasemonkey/gmail_calendar_styler.user.js
@@ -11,5 +11,6 @@ GM_addStyle(
   '.details-container { color: #444 !important; background-color: #fff !important; }' +
   '.date-label { color: #fff; background-color: #555 !important; }' +
   '.day { color: #444; background-color: #f5f5f5 !important; }' +
-  '.menu-link { color: #5BA9FF; font-weight: bold; text-decoration: none; }'
+  '.menu-link { color: #5BA9FF; font-weight: bold; text-decoration: none; }' +
+  '.menu-arrow { color: #5BA9FF; }'
 );
